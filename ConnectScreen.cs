9596a55b315a2a8e6118ebba4eec6b90abf0747c
Florescu Andrei <andrei.florescu.00@gmail.com>
2021-03-23 14:50:38 +0200

    Initial Forms
    Added Main Menu, Main Screen, Help and Connect Forms

--- ConnectScreen.cs
+++ ConnectScreen.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FoxAndHounds
 {
-    public partial class Form1 : Form
+    public partial class ConnectScreen : Form
     {
-        public Form1()
+        public ConnectScreen()
         {
             InitializeComponent();
         }
--- HelpScreen.Designer.cs
+++ HelpScreen.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace FoxAndHounds
 {
-    partial class Form1
+    partial class HelpScreen
     {
         /// <summary>
         /// Required designer variable.
@@ -32,10 +32,9 @@ namespace FoxAndHounds
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "HelpScreen";
         }
 
         #endregion
     }
 }
-
