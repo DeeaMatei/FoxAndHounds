721843da474659dbae727c5d9cccb750b3ad51ae
Florescu Andrei <andrei.florescu.00@gmail.com>
2021-05-27 21:48:02 +0300

    choose player dialog UI

--- FoxAndHounds/ChoosePlayer.Designer.cs
+++ FoxAndHounds/ChoosePlayer.Designer.cs
@@ -36,9 +36,10 @@ namespace FoxAndHounds
             // foxBtn
             // 
             this.foxBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.foxBtn.Location = new System.Drawing.Point(57, 169);
+            this.foxBtn.Location = new System.Drawing.Point(32, 53);
+            this.foxBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.foxBtn.Name = "foxBtn";
-            this.foxBtn.Size = new System.Drawing.Size(193, 91);
+            this.foxBtn.Size = new System.Drawing.Size(129, 59);
             this.foxBtn.TabIndex = 0;
             this.foxBtn.Text = "Fox";
             this.foxBtn.UseVisualStyleBackColor = true;
@@ -47,9 +48,10 @@ namespace FoxAndHounds
             // houndsBtn
             // 
             this.houndsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.houndsBtn.Location = new System.Drawing.Point(394, 169);
+            this.houndsBtn.Location = new System.Drawing.Point(215, 53);
+            this.houndsBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.houndsBtn.Name = "houndsBtn";
-            this.houndsBtn.Size = new System.Drawing.Size(193, 91);
+            this.houndsBtn.Size = new System.Drawing.Size(129, 59);
             this.houndsBtn.TabIndex = 1;
             this.houndsBtn.Text = "Hounds";
             this.houndsBtn.UseVisualStyleBackColor = true;
@@ -57,11 +59,12 @@ namespace FoxAndHounds
             // 
             // ChoosePlayer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(647, 482);
+            this.ClientSize = new System.Drawing.Size(391, 176);
             this.Controls.Add(this.houndsBtn);
             this.Controls.Add(this.foxBtn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ChoosePlayer";
             this.Text = "ChoosePlayer";
             this.ResumeLayout(false);
